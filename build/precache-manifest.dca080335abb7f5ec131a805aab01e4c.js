2d547f4aea9221bb3f2415219dfb1d527ba005b8
Amit Patil <amitgpatil215@gmail.com>
2022-05-09 00:44:15 +0530

    build folder

---
+++ build/precache-manifest.dca080335abb7f5ec131a805aab01e4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5769f6ed8d361e03ca42",
+    "url": "/static/css/main.1987d647.chunk.css"
+  },
+  {
+    "revision": "5769f6ed8d361e03ca42",
+    "url": "/static/js/main.5769f6ed.chunk.js"
+  },
+  {
+    "revision": "673803036639a0ae2a5c",
+    "url": "/static/js/1.67380303.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0976afde75cee0469ba325e042194236",
+    "url": "/index.html"
+  }
+];
